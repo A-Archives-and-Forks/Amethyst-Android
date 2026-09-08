f0e225721b90919807f45a68eed169e02fa4c2c8
tomikun <60690056+alexytomi@users.noreply.github.com>
2026-09-08 22:06:35 +0800

    fix(MioLibPatcher): Only enable patches if needed

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -7,6 +7,7 @@ import static net.kdt.pojavlaunch.Architecture.is64BitsDevice;
 import static net.kdt.pojavlaunch.Tools.LOCAL_RENDERER;
 import static net.kdt.pojavlaunch.Tools.NATIVE_LIB_DIR;
 import static net.kdt.pojavlaunch.Tools.currentDisplayMetrics;
+import static net.kdt.pojavlaunch.Tools.sAsmVersion;
 import static net.kdt.pojavlaunch.Tools.shareLog;
 import static net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_DUMP_SHADERS;
 import static net.kdt.pojavlaunch.prefs.LauncherPreferences.PREF_VSYNC_IN_ZINK;
@@ -386,7 +387,18 @@ public class JREUtils {
         userArgs.add("-XX:ActiveProcessorCount=" + java.lang.Runtime.getRuntime().availableProcessors());
         // Adds/changes methods for compatibility
         userArgs.add("-javaagent:"+new File(Tools.DIR_DATA,"MioLibPatcher/MioLibPatcher.jar").getAbsolutePath());
-        userArgs.add("-Dmiolibpatcher.alc10=true");
+        // Only needed for lwjglx
+        if (Tools.iLwjglVersion <= 299) userArgs.add("-Dmiolibpatcher.alc10=true");
+
+        if (Tools.sAsmVersion != null) {
+            // We override it with 5.0.4 and no forge version ever used 5.0.4, only 5.0.3
+            // This exists for AE1. If any other buggy mods come up, let's enable it for them too.
+            if (Integer.parseInt(Tools.sAsmVersion[0]) == 5 &&
+                    Integer.parseInt(Tools.sAsmVersion[1]) == 0 &&
+                    Integer.parseInt(Tools.sAsmVersion[2]) == 4) {
+                userArgs.add("-Dmiolibpatcher.asmBackport=true");
+            }
+        }
 
         userArgs.addAll(JVMArgs);
         activity.runOnUiThread(() -> Toast.makeText(activity, activity.getString(R.string.autoram_info_msg,LauncherPreferences.PREF_RAM_ALLOCATION), Toast.LENGTH_SHORT).show());
